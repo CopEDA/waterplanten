442ad8daf1cadf44a18e3fd11c1b45d9ff67569f
Johan van Tent <tentvanjohan@hotmail.com>
2025-10-01 10:18:29 +0200

    omzetting naar parquet

---
+++ data-raw/accdb_to_parquet.R
@@ -0,0 +1,29 @@
+library(arrow)
+library(dplyr)
+
+con <- DBI::dbConnect(odbc::odbc(), 
+                      .connection_string = paste0(
+                        "Driver={Microsoft Access Driver (*.mdb, *.accdb)};", 
+                        "DBQ=data-raw/Waterplanten en Waterkwaliteit 2015-2021_def.accdb;" 
+                      ))
+
+tabellen_raw <- DBI::dbListTables(con)
+
+tabellen <- tabellen_raw[stringr::str_detect(tabellen_raw, "^MSys", negate = TRUE)]
+
+for (tabelnaam in tabellen){
+
+# tabelnaam <- "Vegetation data"
+tabel <- dplyr::tbl(con, tabelnaam)
+
+tabelnaam2 <- stringr::str_remove(tabelnaam, ":")
+
+tabel %>% 
+  collect() %>% 
+  write_parquet(paste0("data-raw/parquet/",tabelnaam2, ".parquet"))
+
+
+}
+DBI::dbDisconnect(con)
+
+# open_dataset("data-raw/Vegetation data.parquet") %>% collect() %>% View()

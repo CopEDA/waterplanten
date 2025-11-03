cdcddb011af6c00d8fe9d621fe4c0e66d7109f8f
Johan van Tent <tentvanjohan@hotmail.com>
2025-11-03 14:33:01 +0100

    aanpassing datasets naar tibble

--- data-raw/create_datasets.R
+++ data-raw/create_datasets.R
@@ -13,7 +13,7 @@ for (tabelnaam in tabellen){
   
   
   print(tabel_objectnaam)
-  tabel <- arrow::open_dataset(tabelnaam) |> dplyr::collect()
+  tabel <- arrow::open_dataset(tabelnaam) |> dplyr::collect() %>% tibble::as_tibble()
   
   assign(tabel_objectnaam, tabel, envir = .GlobalEnv)
   
